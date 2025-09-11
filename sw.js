8a2ddebdcc889ed6a989bbc35e3e62c7a23e87a7
JUANSEALVARADO <157227200+JUANSEALVARADO@users.noreply.github.com>
2025-09-11 17:46:36 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "reporte-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// Instalar y guardar en caché
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Servir desde caché si no hay conexión
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
